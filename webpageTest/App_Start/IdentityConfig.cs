ec0f81a8674f729ac4d46d92aa9963e1f04d9b65
Sebastian <sebastian.kortkowski@gmail.com>
2021-01-03 23:50:34 +0100

    Removing unneeded code

--- webpageTest/App_Start/IdentityConfig.cs
+++ webpageTest/App_Start/IdentityConfig.cs
@@ -47,17 +47,14 @@ namespace webpageTest
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                RequireUniqueEmail = false
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireDigit = true
             };
 
             // Configure user lockout defaults
