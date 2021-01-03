4282cc4c3e4c44933949cc01a5fee3e72174f84d
Sebastian <sebastian.kortkowski@gmail.com>
2021-01-03 17:01:38 +0100

    Add project files.

---
+++ webpageTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webpageTest.Startup))]
+namespace webpageTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
